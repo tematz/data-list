aa1684644134cf30ed502682084c6a3564618281
Matheus Vieira <matvno@gmail.com>
2024-02-22 16:17:40 -0300

    feat: list, filters & pagination

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import './index.css'
 import { App } from './app'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
 const queryClient = new QueryClient()
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <RouterProvider router={router} />
     </QueryClientProvider>
   </React.StrictMode>
 )
